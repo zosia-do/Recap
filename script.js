5b901482d8994dc9b47dd5630612b2cf2e9ca82f
zosia-do <zosia7.do@gmail.com>
2025-11-04 00:11:33 +0100

    final project javascript

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
  document.addEventListener("DOMContentLoaded", function(){
-     const IO = new AdafruitIO(username, key);
+    const IO = new AdafruitIO(username, key);
+    
+    
     const temperatureFeed = "temperature";
-    const humidityFeed = "humidity";
-    const co2Feed = "co2";
-
     const temperatureDisplay = document.getElementById("temp-value");
+    const ringTempDisplay = document.getElementById("ring-temp-value");
+    
+    const humidityFeed = "humidity";
     const humidityDisplay = document.getElementById("humidity-value");
-    const co2Display = document.getElementById("co2-value");
+    const ringHumidityDisplay = document.getElementById("ring-humidity-value");
 
     function updateTemperature() {
         IO.getData(temperatureFeed, function({ json }) {
             if (Array.isArray(json) && json.length > 0) {
-                // Use the latest value (first item)
                 let latest = json[0];
                 let tempValue = latest.value;
                 temperatureDisplay.textContent = `${tempValue} °C`;
+                if (ringTempDisplay){
+                    ringTempDisplay.textContent = tempValue;
+                }
             } else {
                 temperatureDisplay.textContent = "-- °C";
+                if (ringTempDisplay){
+                    ringTempDisplay.textContent = "-- °C";
+                }
             }
         });
     }
-
     updateTemperature();
-    setInterval(updateTemperature, 60000);
+    setInterval(updateTemperature, 20000);
+
+    function updateHumidity(){
+        IO.getData(humidityFeed, function({json}){
+            if(Array.isArray(json) && json.length > 0){
+                let latest=json[0];
+                let humidityValue = latest.value;
+                humidityDisplay.textContent = `${humidityValue} %`;
+                if(ringHumidityDisplay){
+                    ringHumidityDisplay.textContent = humidityValue;
+                }
+            } else {
+                humidityDisplay.textContent = "-- %";
+                if (ringHumidityDisplay){
+                    ringHumidityDisplay.textContent = "--%"
+                }
+            }
+        })
+    }
+    updateHumidity();
+    setInterval(updateHumidity, 20000);
 
 });
 
